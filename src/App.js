8c5f39235a71249d9bef58e776457b88402ef578
Swetha R <swetharamasamy378@gmail.com>
2024-10-02 17:33:26 +0530

    front end
    front end

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+import axios from 'axios';
+import './App.css'; // Import your CSS file
+
+const socket = io('http://localhost:5001'); // Ensure this URL points to your backend server
+
+function App() {
+  const [score, setScore] = useState({ batting_team: 'India', runs: 0, wickets: 0 });
+
+  useEffect(() => {
+    
+    const handleScoreUpdate = (updatedScore) => {
+      console.log(updatedScore);
+      setScore(updatedScore);
+    };
+    socket.on('scoreUpdate', handleScoreUpdate);
+    return () => {
+      socket.off('scoreUpdate', handleScoreUpdate); 
+    };
+  }, []); 
+  
+  const fetchScores = async () => {
+    const batting_team = 'India';
+    try {
+      const response = await axios.get('http://localhost:5001/scores', {
+        params: { batting_team },
+      });
+      setScore(response.data.data);
+    } catch (error) {
+      console.error('Error fetching scores:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchScores(); // Fetch scores on component mount
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Live Score Update</h1>
+      <h1>Batting Team: {score.batting_team}</h1>
+      <div className="scoreboard">
+        <h2>{score.runs}/{score.wickets}</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
